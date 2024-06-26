b6393ca907060e487f0a34657fc4085cf63b99b0
KyuhyunK <kim.kyuhyun321@gmail.com>
2024-06-26 10:37:32 -0400

    rapid api sql

--- amazonapi.py
+++ amazonapi.py
@@ -2,14 +2,6 @@ import requests
 import json
 from config import RAPIDAPI_KEY
 
-def get_amazon_data(endpoint, params):
-    url = f"https://real-time-amazon-data.p.rapidapi.com/{endpoint}"
-    headers = {
-        "X-RapidAPI-Key": RAPIDAPI_KEY,
-        "X-RapidAPI-Host": "real-time-amazon-data.p.rapidapi.com"
-    }
-    response = requests.get(url, headers=headers, params=params)
-    return response.json()
 
 def validate_sql_columns(sql_query, valid_columns):
     sql_keywords = {'SELECT', 'AS', 'FROM', 'WHERE', 'GROUP', 'BY', 'ORDER', 'DESC', 'LIMIT', 'SUM', 'AVG', 'COUNT'}
@@ -24,20 +16,22 @@ def validate_sql_columns(sql_query, valid_columns):
 
 
 def invoke_amazon_api(endpoint, params):
-    url = f"https://real-time-amazon-data.p.rapidapi.com/{endpoint}"
+    url = f"https://ai-query2.p.rapidapi.com/{endpoint}"
     headers = {
         "X-RapidAPI-Key": RAPIDAPI_KEY,
-        "X-RapidAPI-Host": "real-time-amazon-data.p.rapidapi.com"
+        "X-RapidAPI-Host": "ai-query2.p.rapidapi.com",
+        "Content-Type": "application/json"
     }
-    response = requests.get(url, headers=headers, params=params)
+    response = requests.post(url, headers=headers, json=params)
     data = response.json()
-    return data.get('query', 'No valid query found')
+    return data
 
 def invoke_amazon_response(prompt):
-    endpoint = "endpoint_for_your_query"  # Specify the endpoint according to your API
-    params = {"query": prompt}  # Set parameters for your API request
+    endpoint = "optimize_sql_query"
+    params = {"prompt": prompt, "model": "oa_v3_16k", "data_source_type": "postgresql"}
     data = invoke_amazon_api(endpoint, params)
     return data
+
 
 
 def invoke_chain(user_question, valid_columns):
@@ -45,7 +39,7 @@ def invoke_chain(user_question, valid_columns):
     sql_query_prompt = f"Generate a SQL query for the following question: {user_question}. The default table name is 'aggregate_profit_data', unless specified otherwise use this table name. Ensure the query includes the table name and the 'FROM' keyword. Use only valid columns from the following list: {', '.join(valid_columns)}. Keep your response concise and easy to understand."
     
     # Use the Amazon API to generate the SQL query
-    generated_sql_query = invoke_amazon_api("your_endpoint_for_sql_generation", {"query": sql_query_prompt})
+    generated_sql_query = invoke_amazon_api("generate_sql", {"query": sql_query_prompt})
     
     print("Generated SQL Query:", generated_sql_query)
 
