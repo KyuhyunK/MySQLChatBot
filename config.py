7124241a3cc15762f6ccd5c9ff810ad4b9c18416
KyuhyunK <kim.kyuhyun321@gmail.com>
2024-06-27 08:44:46 -0400

    llama path

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import os
 
-LLAMA_MODEL_PATH = "C:/Users/SamuelCho/Desktop/Llama/"
+LLAMA_MODEL_PATH =  "C:\\Users\\SamuelCho\\Desktop\\Llama"
 MYSQL_HOST = st.secrets["mysql"]["MYSQL_HOST"]
 MYSQL_USER = st.secrets["mysql"]["MYSQL_USER"]
 MYSQL_PASSWORD = st.secrets["mysql"]["MYSQL_PASSWORD"]
